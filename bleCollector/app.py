9d2571af897595ae24bf1f7f38e904530d1e3f76
kaunghtetsan275 <kevin31.kj@gmail.com>
2023-03-16 14:08:12 +0700

    Updated app.py and docker-compose.yml

--- bleCollector/app.py
+++ bleCollector/app.py
@@ -3,6 +3,7 @@ import asyncio
 from pymongo import MongoClient
 from dotenv import load_dotenv
 import os
+from datetime import datetime
 
 class bleCollector:
    def __init__(self):
@@ -15,10 +16,16 @@ class bleCollector:
 
    async def co_mongo(self):
       self.mongoClient = MongoClient(os.environ['MONGODB_URI'], 27017)
+      ble_db = self.mongoClient.blecollector # Database
+      hb_collection = ble_db.heart_beat # Collection
+      count = 0
       while self.running:
          print("MongoDB is running")
+         beat = {"timestamp":datetime.now(),"gibberish":"constant", "count":count} # Document record
+         hb_collection.insert_one(beat) # Insert document into collection
+         count += 1
          await asyncio.sleep(1)
-   
+
    async def co_mqtt(self):
       self.mqttClient = mqtt.Client()
       self.mqttClient.on_message = self.on_message
